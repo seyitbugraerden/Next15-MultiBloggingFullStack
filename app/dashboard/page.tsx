629664d3d153728a3dfe1d076c1296e8e6fe01b8
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-12 21:51:47 +0300

    creating dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardIndexPage = () => {
+  return (
+    <div>
+      <h1>Hello from Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardIndexPage;
