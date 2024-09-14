a871fc3be5c7fae50194843cc7343e717f30ae6c
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-14 17:57:48 +0300

    page update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const DashboardIndexPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
 
   return (
     <div>
