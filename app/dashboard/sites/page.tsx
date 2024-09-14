f03c1506316d1df27b02552ccb2f7ffa82abf8dd
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-14 13:31:59 +0300

    supabase sites data

--- app/dashboard/sites/page.tsx
+++ app/dashboard/sites/page.tsx
@@ -1,9 +1,40 @@
+import prisma from "@/app/utils/db";
 import { EmptyState } from "@/components/dashboard/EmptyState";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import Defaultimage from "@/public/default.png";
+
+async function getData(userId: string) {
+  const data = await prisma.site.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      creadetAt: "desc",
+    },
+  });
+
+  return data;
+}
 
 export default async function SitesRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+  const data = await getData(user.id);
   return (
     <>
       <div className="flex w-full justify-end">
@@ -13,13 +44,44 @@ export default async function SitesRoute() {
           </Link>
         </Button>
       </div>
-      <EmptyState
-        title="You dont have any Sites created"
-        description="You currently dont have any Sites. Please create some so that you can
+
+      {data === undefined || data.length === 0 ? (
+        <EmptyState
+          title="You dont have any Sites created"
+          description="You currently dont have any Sites. Please create some so that you can
         see them right here!"
-        buttonText="Create Site"
-        href="/dashboard/sites/new"
-      />
+          buttonText="Create Site"
+          href="/dashboard/sites/new"
+        />
+      ) : (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
+          {data.map((item) => (
+            <Card key={item.id}>
+              <Image
+                src={item.imageUrl ?? Defaultimage}
+                alt={item.name}
+                className="rounded-t-lg object-cover w-full h-[200px]"
+                width={400}
+                height={200}
+              />
+              <CardHeader>
+                <CardTitle className="truncate">{item.name}</CardTitle>
+                <CardDescription className="line-clamp-3">
+                  {item.description}
+                </CardDescription>
+              </CardHeader>
+
+              <CardFooter>
+                <Button asChild className="w-full">
+                  <Link href={`/dashboard/sites/${item.id}`}>
+                    View Articles
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
     </>
   );
 }
