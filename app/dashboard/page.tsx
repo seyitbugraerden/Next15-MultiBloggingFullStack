1e946f27f1e73340813f5e177d933862105fffcb
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-16 00:22:41 +0300

    middleware creaed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,6 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 import React from "react";
 
 const DashboardIndexPage = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user) {
-    return redirect("/api/auth/login");
-  }
-
   return (
     <div>
       <h1>Hello from Dashboard</h1>
